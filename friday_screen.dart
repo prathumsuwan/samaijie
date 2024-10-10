6a9988d117cb316c94f9d4282151b7d153b45d28
prathumsuwan <116155060+prathumsuwan@users.noreply.github.com>
2024-10-10 23:20:44 +0700

    Add files via upload

---
+++ friday_screen.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+import 'package:khongmongkol/screen/wid_flower.dart';
+import 'package:khongmongkol/screen/wid_khongmon.dart';
+import 'package:khongmongkol/screen/wid_jewel.dart';
+import 'package:khongmongkol/screen/structure.dart';
+import 'item_screen.dart'; // Assuming this file contains your Item class and itemData list.
+
+class FridayScreen extends StatelessWidget {
+  const FridayScreen({super.key});
+
+  // Function to navigate to the Khongmong Detail screen
+  void khongMong(BuildContext context, String category, String day) {
+    final selectedItem = itemData.firstWhere(
+      (item) =>
+          item.cateogory == category &&
+          item.day == day, // แก้ไข cateogory เป็น category
+      orElse: () =>
+          Item('', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+    );
+
+    if (selectedItem.name.isNotEmpty) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              WidKhongmon(item: selectedItem, day: day), // เพิ่มการส่งค่า day
+        ),
+      );
+    } else {
+      // If no item is found, show a message
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Item not found"),
+          content: const Text("No item matches this day and category."),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("OK"),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  // Function to navigate to the Flower Detail screen
+  void flower(BuildContext context, String category, String day) {
+    final selectedItem = itemData.firstWhere(
+      (item) =>
+          item.cateogory == category &&
+          item.day == day, // แก้ไข cateogory เป็น category
+      orElse: () =>
+          Item('', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+    );
+
+    if (selectedItem.name.isNotEmpty) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              WidFlower(item: selectedItem, day: day), // เพิ่มการส่งค่า day
+        ),
+      );
+    } else {
+      // If no item is found, show a message
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Item not found"),
+          content: const Text("No item matches this day and category."),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("OK"),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  // Function to navigate to the KhongMon (Holy Item) Detail screen, filtering items by category and day
+  void jewel(BuildContext context, String category, String day) {
+    final selectedItem = itemData.firstWhere(
+      (item) =>
+          item.cateogory == category &&
+          item.day == day, // แก้ไข cateogory เป็น category
+      orElse: () =>
+          Item('', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+    );
+
+    if (selectedItem.name.isNotEmpty) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) =>
+              WidJewel(item: selectedItem, day: day), // เพิ่มการส่งค่า day
+        ),
+      );
+    } else {
+      // If no item is found, show a message
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Item not found"),
+          content: const Text("No item matches this day and category."),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("OK"),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Structure(
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/wallpaper.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            // Header with back button and title
+            Row(
+              children: [
+                Container(
+                  width: 40,
+                  height: 40,
+                  color: const Color(0xFF8B5F32),
+                  child: IconButton(
+                    icon: const Icon(Icons.keyboard_arrow_left,
+                        color: Colors.white),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Text(
+                    'วันจันทร์ (Monday)',
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+
+            // Main Image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/mon.png',
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // Description text
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [Color(0xFF000000), Color(0xFF737373)],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                ),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Text(
+                'ตำนานโหราศาสตร์ไทยกล่าวไว้ว่า พระจันทร์...',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // Holy Item Button (KhongMon)
+            _buildCustomButton(
+              context,
+              'assets/khong.png',
+              'วัตถุมงคล',
+              () => khongMong(context, 'thing1', 'fri'),
+            ),
+            const SizedBox(height: 20),
+
+            // Flower Button
+            _buildCustomButton(
+              context,
+              'assets/flowerBG.png',
+              'ดอกไม้มงคล',
+              () => flower(context, 'plant1', 'fri'),
+            ),
+            const SizedBox(height: 20),
+
+            // Jewel Button
+            _buildCustomButton(
+              context,
+              'assets/jewel.png',
+              'อัญมณีมงคล',
+              () => jewel(context, 'gem1', 'fri'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCustomButton(BuildContext context, String imagePath,
+      String label, VoidCallback onPressed) {
+    return SizedBox(
+      width: 400,
+      height: 100,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            colors: [Color(0xFF000000), Color(0xFF8B5F32)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: TextButton(
+          onPressed: onPressed,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Image.asset(imagePath, width: 70, height: 70),
+                    const SizedBox(width: 15),
+                    Text(
+                      label,
+                      style: const TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                ),
+                const Text(
+                  '>',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
